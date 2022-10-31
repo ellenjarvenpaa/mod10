88c36fd1de62f59da8b7e1572f7158d495a0f86b
ellen.jarvenpaa <ellen.jarvenpaa@metropolia.fi>
2022-10-31 13:32:48 +0200

    :-)

--- teht.10.2.py
+++ teht.10.2.py
@@ -22,8 +22,22 @@ class Hissi:
             if self.atm == kerros:
                 print(self.atm)
 
+class Talo:
+    def __init__(self, alin, ylin, h_lkm):
+        self.alin = alin
+        self.ylin = ylin
+        self.h_lkm = h_lkm
+        self.hissit = []
+        for i in range(1, 5):
+            self.hissit.append(Hissi(1, 10, 1))
+
+    def aja_hissia(self, h_num, kohde):
+
+
 # pääohjelma
 
+t = Talo(1, 10, 4)
 h = Hissi(1, 10, 1)
 kerros = int(input("Mihin kerrokseen haluat?"))
 h.siirry_kerrokseen(kerros)
+t.aja_hissia(kerros)
